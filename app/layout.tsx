cf79e051f1b4bbcf71ddf8b63e7c8def2bd41f2e
henkribeiro <henkribeiro@outlook.com>
2026-02-24 16:06:26 -0300

    feat: enhance styling and theming with Tailwind CSS, add utility functions, and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import {Poppins } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], 
   subsets: ["latin"],
+  variable: "--font-poppins",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
